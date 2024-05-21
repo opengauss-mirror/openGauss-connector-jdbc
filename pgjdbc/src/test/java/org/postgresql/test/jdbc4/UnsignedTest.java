73a3ae800b3f41e9ddd4d4aa8927bc4f821a39b1
zhangting <zt1819798371@163.com>
2024-05-21 11:04:04 +0800

    修复B模式下 uint1/uint2/uint4/uint8 的getObject类型为PGobject与M*不一致问题

--- pgjdbc/src/test/java/org/postgresql/test/jdbc4/UnsignedTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc4/UnsignedTest.java
@@ -15,6 +15,76 @@ import static org.junit.Assert.assertEquals;
 public class UnsignedTest extends BaseTest4 {
 
     /**
+     * test uint1 type
+     * @throws Exception
+     */
+    @Test
+    public void testUint1() throws SQLException {
+        TestUtil.createTable(con, "test_unit1", "id uint1");
+
+        PreparedStatement pstmt = con.prepareStatement("INSERT INTO test_unit1 VALUES (?)");
+        pstmt.setObject(1, 234, Types.SMALLINT);
+        pstmt.executeUpdate();
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT id FROM test_unit1");
+
+        assertTrue(rs.next());
+        Object r1 = rs.getObject(1);
+        assertNotNull(r1);
+        assertEquals(234, r1);
+
+        TestUtil.dropTable(con, "test_unit1");
+    }
+
+    /**
+     * test uint2 type
+     * @throws Exception
+     */
+    @Test
+    public void testUint2() throws SQLException {
+        TestUtil.createTable(con, "test_unit2", "id uint2");
+
+        PreparedStatement pstmt = con.prepareStatement("INSERT INTO test_unit2 VALUES (?)");
+        pstmt.setObject(1, 65518, Types.INTEGER);
+        pstmt.executeUpdate();
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT id FROM test_unit2");
+
+        assertTrue(rs.next());
+        Object r1 = rs.getObject(1);
+        assertNotNull(r1);
+        assertEquals(65518, r1);
+
+        TestUtil.dropTable(con, "test_unit2");
+    }
+
+    /**
+     * test uint4 type
+     * @throws Exception
+     */
+    @Test
+    public void testUint4() throws SQLException {
+        TestUtil.createTable(con, "test_unit4", "id uint4");
+
+        PreparedStatement pstmt = con.prepareStatement("INSERT INTO test_unit4 VALUES (?)");
+        long l = 4294967282L;
+        pstmt.setObject(1, l, Types.BIGINT);
+        pstmt.executeUpdate();
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT id FROM test_unit4");
+
+        assertTrue(rs.next());
+        Object r1 = rs.getObject(1);
+        assertNotNull(r1);
+        assertEquals(l, r1);
+
+        TestUtil.dropTable(con, "test_unit4");
+    }
+
+    /**
      * test uint8 type
      * @throws Exception
      */
